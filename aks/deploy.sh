65a8b3c4c6e2e40aa6616d758580641021b8c9df
Javier Mayorga <javiermayorga@Javiers-MacBook-Pro.local>
2021-09-22 19:59:00 -0600

    add acr and vnet

--- aks/deploy.sh
+++ aks/deploy.sh
@@ -1,4 +1,8 @@
 slot=$1
 timestamp=`date "+%Y%m%d-%H%M%S"`
 resourceGroup=rg-workspacemgmt-dev-001 
+resourceGroupShared=rg-shared-dev-002
+az group create --location eastus --name $resourceGroupShared --tags "Project":"WorkspaceManagement", "Environment":"Dev"
+az group create --location eastus --name $resourceGroup --tags "Project":"WorkspaceManagement", "Environment":"Dev"
+az deployment group create -g $resourceGroupShared --template-file acr-vnetTemplate.json  -n "acr-vnet-${timestamp}"
 az deployment group create -g $resourceGroup --template-file aksTemplate.json --parameters parameters.json -n "aks-${timestamp}"
