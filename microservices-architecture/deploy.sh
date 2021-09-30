d7fcd15752d4bd6d8ceb6e342315a4c2faeb8cbd
jmayorga94 <javi.mayorga1994@outlook.com>
2021-09-30 09:25:40 -0600

    fix resource id for deploying sql database, servicebus and aks

--- microservices-architecture/deploy.sh
+++ microservices-architecture/deploy.sh
@@ -6,5 +6,6 @@ az group create --location eastus --name $resourceGroupShared --tags "Project":"
 az group create --location eastus --name $resourceGroup --tags "Project":"WorkspaceManagement", "Environment":"Dev"
 az deployment group create -g $resourceGroupShared --template-file acr-vnet/acr-vnetTemplate.json  -n "acr-vnet-${timestamp}"
 az deployment group create -g $resourceGroup --template-file aks/aks.json --parameters aks/parameters.json -n "aks-${timestamp}"
-az deployment group create -g $resourceGroup --template-file servicebus/servicebusTemplate.json --parameters /servicebus/parameters.json -n "servicebus-${timestamp}"
+az deployment group create -g $resourceGroup --template-file servicebus/servicebusTemplate.json --parameters servicebus/parameters.json -n "servicebus-${timestamp}"
 az deployment group create -g $resourceGroup --template-file sql-database/sqltemplate.json --parameters Sql-Database/parameters.json      -n "sql-database-${timestamp}"
+ az aks update -n aks-dev-workspacemngmt  -g rg-workspacemgmt-dev  --attach-acr crdevelopmenteastus001
